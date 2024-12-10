0b902d9d1a5d0050271ddf69d71230495d39b618
metaliccode <isan.mh69@gmail.com>
2024-12-10 08:17:21 +0700

    Add: API Products

---
+++ app/Http/Controllers/API/ProductsApiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProductsApiController extends Controller
+{
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+        $products->load('category');
+
+        return response()->json(
+            $products,
+            Response::HTTP_OK
+        );
+    }
+}
